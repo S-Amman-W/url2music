e611f7bf6f323902b0dbb942d5dbdc09277fa606
Amman Waheed <swaheed7517@hwdsb.on.ca>
2018-02-04 04:54:11 -0500

    got url to save to firebase

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import firebase from 'firebase';
 import './App.css';
 import Box from  './components/box.js';
 import URL from './components/url.js';
+import logo from './logo.svg';
+
 class App extends Component {
-  constructor(props) {
-  super(props);
+  constructor() {
+  super();
   this.state = {
-    selectedState: ''
+    URL: ''
   }
-
-
   var config = {
       apiKey: "AIzaSyBV4ISoX0keSocOpnIRNrpzYuvzkRN8TQ0",
       authDomain: "url2sound.firebaseapp.com",
@@ -19,10 +19,32 @@ class App extends Component {
      storageBucket: "",
      messagingSenderId: "907970903449"
  };
-
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
   firebase.initializeApp(config);
-
 }
+  handleChange(e) {
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    }
+
+    handleSubmit(e) {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items');
+    const item = {
+      URL: this.state.URL
+    }
+    itemsRef.push(item);
+    this.setState({
+      URL: item.URL
+    });
+  }
+  // <form onSubmit={this.handleSubmit}>
+  //                <input type="text" name="username" placeholder="URL" onChange={this.handleChange} value={this.state.URL} />
+  //               <button>Save URL</button>
+  //              </form>
+
 
 saveURL = (e) => {
   var a = localStorage.setItem("url", e)
@@ -49,14 +71,17 @@ saveURL = (e) => {
         </div>
         <br/>
           <p>URL to Save : </p>
-          <form name="urlsaver" id="urlForm">
-            <input type="url" name="url" />
-            <div class="URL"><button name="Hello" onClick={() => this.saveURL()}> hi </button></div>
+          <form name="urlsaver" id="urlForm" onSubmit={this.handleSubmit}>
+            <input type="URL" name="URL" onChange={this.handleChange} value={this.state.URL} />
+            <div class="URL"><button onClick =  {() => {
+              this.setState({selectedState: "urls"});
+        //      firebase.database().ref('urls').push({"val":"dgfggffsg"})
+            }}> Hi </button></div>
           </form>
 
             {/* <p>{return this.localStorage.getItem("url");</p> */}
           <br/> <br/>
-              <div class="URL"><button onClick =  {() => {this.setState({selectedState: "urls"})}}> Your URLs </button></div>
+              <div class="URL"></div>
           <br/> <br/>
               <div class="URL"><button onClick =  {() => {this.setState({selectedState: "playlists"})}}> Your Playlists </button></div>
               <Box selectedState={this.state.selectedState}></Box>
